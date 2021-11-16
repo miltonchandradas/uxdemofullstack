7f87a99e5a931fbbd7751ceb60b97f072ff182a8
Milton Chandradas <milton.chandradas@sap.com>
2021-11-16 05:39:27 +0000

    Deploy to CF runtime in SAP BTP...

--- srv/server.js
+++
@@ -1,7 +0,0 @@
-const cds = require("@sap/cds");
-const proxy = require("@sap/cds-odata-v2-adapter-proxy");
-
-cds.on("bootstrap", () => console.log("This custom function was called..."));
-cds.on("bootstrap", (app) => app.use(proxy()));
-
-module.exports = cds.server;
